f777c34fa2de4af02b28526e6470275d29ca5581
James Netherton <jamesnetherton@gmail.com>
2022-01-20 09:03:24 +0000

    Fix application dependencies

--- src/main/java/t/lux/quarkus/azure/MyReactiveMessagingApplication.java
+++ src/main/java/t/lux/quarkus/azure/MyReactiveMessagingApplication.java
@@ -1,29 +1,13 @@
 package t.lux.quarkus.azure;
 
-import io.quarkus.runtime.StartupEvent;
-import org.apache.camel.CamelContext;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import javax.inject.Inject;
 import java.util.concurrent.CompletionStage;
 
 @ApplicationScoped
 public class MyReactiveMessagingApplication {
-
-    @Inject
-    private CamelContext camelContext;
-//
-//    @Inject
-//    @Channel("source-out")
-//    Emitter<String> emitter;
-
-    /** Sends message to the source channel, can be used from a JAX-RS resource or any bean of your application **/
-    void onStart(@Observes StartupEvent ev) {
-        camelContext.start();
-    }
 
     @Incoming("source-in")
     public CompletionStage<Void> consume(Message<String> message) {
